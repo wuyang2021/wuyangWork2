87725b62c4fdee98d97069a19588bdca15e2c913
wuyang <wuyang@163.com>
2022-08-27 10:06:19 +0800

    wuyang02 第一次提交到本地库

--- src/main/java/com/at/javatest/MyJavaTest1.java
+++ src/main/java/com/at/javatest/MyJavaTest1.java
@@ -4,5 +4,6 @@ public class MyJavaTest1 {
     public static void main(String[] args) {
         System.out.println(MyJavaTest1.class);
         System.out.println(MyJavaTest1.class+"2");
+        System.out.println(MyJavaTest1.class+"3");
     }
 }
