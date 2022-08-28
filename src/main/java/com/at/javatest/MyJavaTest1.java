4c38d462cde55150ec4105f0900ac45aeb810251
wuyang2021 <96456243+wuyang2021@users.noreply.github.com>
2022-08-28 12:23:35 +0800

    拉取测试

--- src/main/java/com/at/javatest/MyJavaTest1.java
+++ src/main/java/com/at/javatest/MyJavaTest1.java
@@ -2,11 +2,6 @@ package com.at.javatest;
 
 public class MyJavaTest1 {
     public static void main(String[] args) {
-        System.out.println(MyJavaTest1.class);
-        System.out.println(MyJavaTest1.class+"2");
-        System.out.println(MyJavaTest1.class+"3");
-        System.out.println(MyJavaTest1.class+"master1");
-        System.out.println(MyJavaTest1.class+"wuyang02");
         System.out.println(MyJavaTest1.class+"20220828");
     }
 }
